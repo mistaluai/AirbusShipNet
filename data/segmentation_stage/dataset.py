bfa935b48133fe014a4adfd7d82c4ef721679efa
mohiey <mohamedmohiey507@gmail.com>
2025-03-28 03:08:50 +0200

    Segmentation Dataset.py final touch

--- data/segmentation_stage/dataset.py
+++ data/segmentation_stage/dataset.py
@@ -4,6 +4,9 @@ import pandas as pd
 
 from torch.utils.data import Dataset
 from torchvision.transforms import v2
+from torchvision.tv_tensors import Mask
+
+from PIL import Image, UnidentifiedImageError
 
 class SegmentationDataset(Dataset):
     def __init__(self, df: pd.DataFrame, original_shape: tuple[int, int] = (768, 768), transform=None):
@@ -14,6 +17,33 @@ class SegmentationDataset(Dataset):
             v2.ToImage(),
         ]
     )
+        self.image_dtype_transform = v2.ToDtype(torch.float32, scale=True)
+        self.mask_dtype_transform = v2.ToDtype(torch.float32, scale=False)
+
+    def __len__(self):
+        return len(self.df)
+
+    def __getitem__(self, idx):
+        image_path, rle_list = self.df.iloc[idx]
+        combined_mask = torch.zeros(self.original_shape, dtype=torch.uint8)
+
+        for rle_mask_str in rle_list:
+            single_mask = self.rle_decode(rle_mask_str)
+            combined_mask = torch.logical_or(combined_mask, single_mask.to(combined_mask.device))
+
+        mask = Mask(combined_mask.to(torch.uint8))
+
+        try:
+            image = Image.open(image_path).convert('RGB')
+            image, mask = self.transform(image, mask)
+
+            image = self.image_dtype_transform(image)
+            mask = self.mask_dtype_transform(mask)
+        except (OSError, UnidentifiedImageError):
+            next_idx = idx + 1
+            return self.__getitem__(next_idx)
+
+        return image, mask
 
     @staticmethod
     def rle_decode(mask_rle: str, shape: tuple[int, int] = (768, 768)) -> torch.Tensor:
@@ -38,12 +68,3 @@ class SegmentationDataset(Dataset):
 
         # Return the mask as a tensor of type uint8
         return (mask > 0).to(torch.uint8) 
-
-    def __len__(self):
-        return len(self.df)
-
-    def __getitem__(self, idx):
-        image_path, rle_list = self.df.iloc[idx]
-        combined_mask = torch.zeros(self.original_shape, dtype=torch.uint8)
-
-        return
